bce17928cb7db35b1481f59bcac92191675cd82a
Duy Trần <khnhduytrn446guitarr@gmail.com>
2023-03-23 22:52:54 +0700

    add Content layout

--- momo-charity/src/App.js
+++ momo-charity/src/App.js
@@ -242,7 +242,7 @@ function App() {
       {/* Ví Nhân Ái - Thiện nguyện mỗi ngày */}
       <section id="block-3">
         <div id="text-header-2" className="text-center p-5">
-          <h2>Ví Nhân Ái - Thiện nguyện mỗi ngày</h2>
+          <h2><b>Ví Nhân Ái - Thiện nguyện mỗi ngày</b></h2>
         </div>
         <div  className="container">
           <div className="row">
@@ -311,7 +311,55 @@ function App() {
           <h5 id="text-header-5" className="text-center">Các tổ chức nhân đạo Phi Lợi Nhuận đồng hành cùng Ví MoMo giúp đỡ các hoàn cảnh khó khăn<br /> trên khắp cả nước.</h5>
         </div>
       </section>
-      <Partner />
+      <Partner/>
+
+      {/* Khi thiện nguyện là nguồn hạnh phúc */}
+      <section id="block-5">
+        <div id="text-header-2" className="text-center p-5">
+            <h2><b>Khi thiện nguyện là nguồn hạnh phúc</b></h2>
+        </div>
+        
+        <div className="container">
+          <div className="row">
+            <div className="col"></div>
+            <div id="col-paragraph" className="col-10">
+              <p id="paragraph">Đời sống xã hội ngày càng được nâng cao cũng là lúc người ta phát sinh nhu cầu muốn được chia sẻ, được đóng góp cho xã hội, giúp đỡ những cộng đồng yếu thế, những hoàn cảnh khó khăn.<p></p>
+
+Làm việc thiện không chỉ là sự cho đi, sự sẻ chia giá trị mà còn giúp mỗi người nhận về niềm vui, cảm xúc tích cực, ý nghĩa. Với tôn chỉ “người dùng hạnh phúc”, MoMo quyết định khai thác “nguồn hạnh phúc” đầy ý nghĩa là hoạt động thiện nguyện. <p></p>
+
+Có sẵn ưu thế nền tảng công nghệ cao, hướng tới sự thuận tiện và minh bạch, Trái tim MoMo liên kết với những quỹ thiện nguyện, những dự án cộng đồng, trở thành cầu nối cho các nhà hảo tâm. <p></p>
+
+Việc quyên góp tiền từ thiện trở nên đơn giản, chỉ gói gọn trong vài thao tác nhỏ trên điện thoại thông minh. Các thông tin về đóng góp, tài trợ được công khai và cập nhật liên tục, người dùng có thể dễ dàng tra cứu. <p></p>
+
+Từ đó, những dự án đầy ý nghĩa như Sức mạnh 2000; Quỹ Hy Vọng; Hoa Chia Sẻ… nhận được thêm nhiều nguồn lực đóng góp.<p></p>
+
+Một trong những dự án thiện nguyện nổi bật được liên kết với Ví MoMo phải kể đến Sức mạnh 2000, dự án quyên góp tiền xây trường học vùng cao với ý tưởng “xin” nhà hảo tâm 2.000 đồng mỗi ngày.<p></p>
+
+Anh Hoàng Hoa Trung, đại diện dự án từng nhận xét, MoMo là nền tảng phù hợp nhất bởi “chẳng có hệ thống nào khác giúp tự trừ 2.000 đồng mỗi ngày”.<p></p>
+
+Tầm nhìn của dự án Sức mạnh 2000 cũng tương đồng với thông điệp về thiện nguyện của MoMo là “một ngàn đồng cũng đáng quý”. Với mức quyên góp tối thiểu 1.000 đồng, người dùng MoMo có thể tận dụng tiền lẻ sau mỗi lần giao dịch để làm từ thiện, chỉ là số tiền nhỏ nhưng đem lại những giá trị lớn lao, cao cả.<p></p>
+
+Đối với tính năng Heo đất MoMo, người dùng nhận được Heo vàng sau mỗi lần giao dịch, chuyển tiền hoặc những hoạt động như đi bộ, dậy sớm, giải câu đố, tương tác với bạn bè… Heo Vàng được quy đổi thành tiền và quyên góp vào các dự án thiện nguyện.<p></p>
+
+Ngoại trừ chi phí chuyển khoản ngân hàng, 100% số tiền quyên góp được từ Heo đất MoMo được chuyển tới các tổ chức bảo trợ, tổ chức từ thiện. Trong trường hợp gây quỹ không thành công (không đủ số Heo vàng), MoMo vẫn chuyển số tiền tương ứng với số Heo vàng đã nhận được hoặc có thể gia hạn thời gian gây quỹ.<p></p>
+
+Ra mắt từ tháng 9/2019, những chú Heo Vàng đã đồng hành cùng nhiều nhà tài trợ, quyên góp được hơn 45 tỷ đồng tiền mặt và hơn 520 triệu Heo Vàng (tính đến tháng 6/2022, trung bình 1 Heo Vàng trị giá 140đ), hỗ trợ cho hơn 94 nghìn em nhỏ có hoàn cảnh khó khăn, giúp các em được ăn no, được đến trường.<p></p>
+
+“Ngoài số tiền quy đổi, ý nghĩa của Heo đất MoMo còn nằm ở thông điệp: thiện nguyện không phải là điều gì to tát, mà chỉ là công việc hàng ngày, ai ai cũng có thể góp sức, dù chỉ bằng việc đi bộ, dậy sớm, có lối sống lành mạnh. Những điều nhỏ bé đó không chỉ cùng nhau tạo nên sự thay đổi vĩ đại cho cộng đồng, mà còn là nguồn cảm hứng sống tích cực cho mỗi người dùng Ví MoMo”, ông Diệp chia sẻ.<p></p>
+
+Theo : <a href="https://theleader.vn">https://theleader.vn</a>/</p>
+
+            </div>
+            <div className="col"></div>
+
+          </div>
+        </div>
+
+
+
+
+
+      </section>
 
 
       {/* Phần thông tin ở cuối trang */}
