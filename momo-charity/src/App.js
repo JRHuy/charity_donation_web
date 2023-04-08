4cd3c846daa44586b229966486af488efc9c686e
JRHuy <duchuy2001@outlook.com>
2023-04-09 00:50:54 +0700

    update navbar

--- momo-charity/src/App.js
+++ momo-charity/src/App.js
@@ -3,212 +3,26 @@ import BottomCredit from "./BottomCredit";
 import chevronRight from "./chevron-right.svg";
 import Partner from "./Partner";
 import CardItem from "./CardItem";
+import Navbar from "./Navbar";
 
 function App() {
     return (
-        // <div className="App">
-        //   <header className="App-header">
-        //     <img src={logo} className="App-logo" alt="logo" />
-        //     <p>
-        //       Edit <code>src/App.js</code> and save to reload.
-        //     </p>
-        //     <a
-        //       className="App-link"
-        //       href="https://reactjs.org"
-        //       target="_blank"
-        //       rel="noopener noreferrer"
-        //     >
-        //       Learn React
-        //     </a>
-        //   </header>
-        // </div>
         <div>
-            <section id="1">
-                <div className="container">
-                    <nav className="navbar navbar-expand-lg navbar-light">
-                        <a className="navbar-brand" href="#">
-                            <img
-                                src="momo.png"
-                                width="35"
-                                height="35"
-                                alt="logo"
-                            />
-                        </a>
-                        <a className="navbar-brand mb-0 h1" href="#">
-                            Ví Nhân Ái
-                        </a>
-                        <button
-                            className="navbar-toggler"
-                            type="button"
-                            data-toggle="collapse"
-                            data-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                        >
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div
-                            className="collapse navbar-collapse"
-                            id="navbarSupportedContent"
-                        >
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">
-                                        Trái Tim Momo
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">
-                                        Heo Đất Momo
-                                    </a>
-                                </li>
-                                <li className="nav-item dropdown">
-                                    <a
-                                        className="nav-link dropdown-toggle"
-                                        href="#"
-                                        role="button"
-                                        data-bs-toggle="dropdown"
-                                        aria-expanded="false"
-                                    >
-                                        Hoàn Cảnh Quyên Góp
-                                    </a>
-                                    <ul className="dropdown-menu">
-                                        <li>
-                                            <a
-                                                className="dropdown-item"
-                                                href="#"
-                                            >
-                                                Vì Trẻ Em
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                className="dropdown-item"
-                                                href="#"
-                                            >
-                                                Người Già, Người Khuyết Tật
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                className="dropdown-item"
-                                                href="#"
-                                            >
-                                                Bệnh Hiểm Nghèo
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                className="dropdown-item"
-                                                href="#"
-                                            >
-                                                Hoàn Cảnh Khó Khăn
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                className="dropdown-item"
-                                                href="#"
-                                            >
-                                                Hỗ Trợ Giáo Dục
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                className="dropdown-item"
-                                                href="#"
-                                            >
-                                                Đầu Tư Cơ Sở Vật Chất
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                className="dropdown-item"
-                                                href="#"
-                                            >
-                                                Cứu Trợ Động Vật
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                className="dropdown-item"
-                                                href="#"
-                                            >
-                                                Bảo Vệ Môi Trường
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">
-                                        Đối Tác Đồng Hành
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">
-                                        Tin Tức Cộng Đồng
-                                    </a>
-                                </li>
-                                <li className="nav-item dropdown">
-                                    <a
-                                        className="nav-link dropdown-toggle"
-                                        href="#"
-                                        role="button"
-                                        data-bs-toggle="dropdown"
-                                        aria-expanded="false"
-                                    >
-                                        Blog Cuộc Sống
-                                    </a>
-                                    <ul className="dropdown-menu">
-                                        <li>
-                                            <a
-                                                className="dropdown-item"
-                                                href="#"
-                                            >
-                                                Sống Tốt
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                className="dropdown-item"
-                                                href="#"
-                                            >
-                                                Sống Khỏe
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                className="dropdown-item"
-                                                href="#"
-                                            >
-                                                Sống Thông Minh
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a
-                                                className="dropdown-item"
-                                                href="#"
-                                            >
-                                                Sống Vui
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
-            </section>
-            <hr />
+            {/* ------------------ Section 1 ----------------- */}
+            <Navbar/>
 
-            <section>
+            <section id="block-2">
                 <div className="container pb-3">
                     <i className="fa fa-fw fa-home" />
                     <img src={chevronRight} />
                     <span>Ví Nhân Ái</span>
                 </div>
                 <img src="momo_banner.jpg" className="img-fluid" alt="banner" />
+                <div className="container py-3">
+                    <i className="fa fa-fw fa-home" />
+                    <img src={chevronRight} />
+                    <span>Ví Nhân Ái</span>
+                </div>
                 <div className="container p-5">
                     <div className="row">
                         <div className="col-md-6">
@@ -279,7 +93,7 @@ function App() {
                                 </div>
                             </div>
                             <div className="hstack gap-2 pb-3">
-                                <button type="button" className="btn btn-info">
+                                <button type="button" className="btn btn-info" style={{color:"white"}}>
                                     Quyên góp
                                 </button>
                                 <button
---
+++ momo-charity/src/Navbar.js
@@ -0,0 +1,191 @@
+import React, { useEffect, useState } from 'react';
+
+function Navbar() {
+    // const [stickyNav, setStickyNav] = useState('relative');
+
+    // useEffect(() => {
+    //     window.addEventListener('scroll', stickNavbar);
+    //     return () => window.removeEventListener('scroll', stickNavbar);
+    // }, []);
+
+    // const stickNavbar = () => {
+    //     if (window !== undefined) {
+    //         let windowHeight = window.scrollY;
+    //         windowHeight > 150 ? setStickyNav('fixed top-0 left-0 z-50') : setStickyNav('relative');
+    //     }
+    // };
+
+    return (
+        <section id="1" className='sticky-top'>
+            <div className="container-fluid px-0 d-flex justify-content-center" style={{ backgroundColor: "white" }}>
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <a className="navbar-brand" href="#">
+                        <img
+                            src="momo.png"
+                            width="35"
+                            height="35"
+                            alt="logo"
+                        />
+                    </a>
+                    <a className="navbar-brand mb-0 h1" href="#">
+                        Ví Nhân Ái
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerContent" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div
+                        className="collapse navbar-collapse"
+                        id="navbarTogglerContent"
+                    >
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Trái Tim Momo
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Heo Đất Momo
+                                </a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a
+                                    className="nav-link dropdown-toggle"
+                                    href="#"
+                                    role="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
+                                    Hoàn Cảnh Quyên Góp
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li>
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                        >
+                                            Vì Trẻ Em
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                        >
+                                            Người Già, Người Khuyết Tật
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                        >
+                                            Bệnh Hiểm Nghèo
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                        >
+                                            Hoàn Cảnh Khó Khăn
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                        >
+                                            Hỗ Trợ Giáo Dục
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                        >
+                                            Đầu Tư Cơ Sở Vật Chất
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                        >
+                                            Cứu Trợ Động Vật
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                        >
+                                            Bảo Vệ Môi Trường
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Đối Tác Đồng Hành
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Tin Tức Cộng Đồng
+                                </a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a
+                                    className="nav-link dropdown-toggle"
+                                    href="#"
+                                    role="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
+                                    Blog Cuộc Sống
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li>
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                        >
+                                            Sống Tốt
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                        >
+                                            Sống Khỏe
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                        >
+                                            Sống Thông Minh
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            className="dropdown-item"
+                                            href="#"
+                                        >
+                                            Sống Vui
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </section >
+    );
+}
+
+export default Navbar;
