0adb60a91410456c53fd3faf0b13f5ca93cf4fe8
Hoang <howtolol0903@gmail.com>
2023-05-31 10:51:11 +0700

    Adjust PartnerCard

--- momo-charity/src/components/PartnerCard.js
+++ momo-charity/src/components/PartnerCard.js
@@ -1,27 +1,33 @@
 function PartnerCard(props) {
-    return (
-        <>
-            <div className="card" id="card-doitac">
-                {/* make card as a button using stretched-link */}
-                <a href="#" className="card-block stretched-link text-decoration-none">
-                    <div className="row card-body">
-                        <img className="col-sm-3" src={props.partnerImg} style={{ width: 80, height: 60 }} />
-                        <div className="col-sm-9">
-                            <h6 className="card-title" id="doitac">
-                                {props.partnerName}
-                                <br />
-                                <p className="card-text" id="card-description">{props.partnerDescription}</p>
-                            </h6>
-                            <p id="xemchitiet">
-                                Xem chi tiết <i className="fa-solid fa-chevron-right"></i>
-                                <i className="fa-solid fa-chevron-right"></i>
-                            </p>
-                        </div>
-                    </div>
-                </a>
+  return (
+    <>
+      <div className="card" id="card-doitac">
+        {/* make card as a button using stretched-link */}
+        <a href="#" className="card-block stretched-link text-decoration-none">
+          <div className="row card-body">
+            <img
+              className="col-sm-3"
+              src={props.partnerImg}
+              style={{ width: 80, height: 60 }}
+            />
+            <div className="col-sm-9">
+              <h6 className="card-title" id="doitac">
+                {props.partnerName}
+                <br />
+                <p className="card-text text-truncate" id="card-description">
+                  {props.partnerDescription}
+                </p>
+              </h6>
+              <div className="" id="xemchitiet">
+                Xem chi tiết <i className="fa-solid fa-chevron-right"></i>
+                <i className="fa-solid fa-chevron-right"></i>
+              </div>
             </div>
-        </>
-    )
+          </div>
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default PartnerCard;
