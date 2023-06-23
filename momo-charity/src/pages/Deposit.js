909ccaa8c5647c629a89590591dc297e4f8520c8
JRHuy <duchuy2001@outlook.com>
2023-06-23 22:51:10 +0700

    update for transactions functions
    - ask for password to confirm before making any transactions.
    - display error notification when user inputs incorrect password.

--- momo-charity/src/pages/Deposit.js
+++ momo-charity/src/pages/Deposit.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 import { useState } from "react";
 import { NumericFormat } from "react-number-format";
 import { Link, useNavigate } from "react-router-dom";
-import style from "../styles/style.module.css"
+import style from "../styles/style.module.css";
 
 function Deposit() {
     const [error, setError] = useState(false);
@@ -10,6 +10,13 @@ function Deposit() {
     const [money, setMoney] = useState("");
     const [id, setId] = useState("");
     const [success, setSuccess] = useState(false);
+
+    // handle check password to confirm transation
+    const [showConfirmDialog, setShowConfirmDialog] = useState(false);
+    const [userPhoneNum, setUserPhoneNum] = useState(null);
+    const [userPassword, setUserPassword] = useState(null);
+    const [wrongPassword, setWrongPassword] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
     const navigate = useNavigate();
 
@@ -29,16 +36,51 @@ function Deposit() {
         navigate(-1);
     }
 
-    const onSubmit = (e) => {
+    // pop-up to ask for credentials
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+    
+    const handleTransationClick = (e) => {
         e.preventDefault();
+        setUserPhoneNum(user.phoneNum);
+        setShowConfirmDialog(true);
+    };
+
+    const onPasswordChange = (e) => {
+        setUserPassword(e.target.value);
+    }
+
+    const handleConfirmTransaction = () => {
+        const user = {
+            'phoneNum': userPhoneNum,
+            'userPassword': userPassword
+        }
+        Axios.post('http://localhost:8080/api/auth/login', user)
+            .then((res) => {
+                setShowConfirmDialog(false);
+                onSubmit();
+            }).catch(err => {
+                console.log(err);
+                setWrongPassword(true);
+            });
+    };
+
+    const handleCancel = () => {
+        setShowConfirmDialog(false);
+    };
+
+    const onSubmit = () => {
+        // e.preventDefault();
+        // console.log("Submit actions");
         if (money.length === 0) {
             setError(true);
         } else {
+            // console.log("Perform deposit");
             Axios.post("http://localhost:8080/api/user/deposit", data)
                 .then(res => {
                     setSuccess(true);
                     setMoney("");
-                    console.log(res.data);
                 }).catch(err => console.log(err));
         }
     }
@@ -49,23 +91,23 @@ function Deposit() {
                 <div className="form4enter">
                     <div className="row justify-content-center">
                         <h3 id='title_page' className="text-center text-secondary mt-5 mb-3">THÊM TIỀN VÀO TÀI KHOẢN</h3>
-                        <form method="POST" onSubmit={(e) => onSubmit(e)}>
+                        <form method="POST" onSubmit={(e) => handleTransationClick(e)}>
                             <div className="form-group">
                                 <label style={{ display: "flex" }} htmlFor="money">Nhập số tiền</label>
                                 <NumericFormat name="money" id="money" value={money} suffix="đ" displayType="input" type="tel" thousandSeparator="." decimalSeparator="," placeholder="0đ" className={style.money} onValueChange={(values, e) => {
-                                    const {formattedValue, value, floatValue} = values;
-                                    console.log(values.value);
+                                    const { value } = values;
+                                    // console.log(values.value);
                                     onInputChange(value);
                                 }} />
                                 {/* <input type="number" className="form-control" placeholder="0 VND" value={money} onChange={(e) => onInputChange(e)} /> */}
                             </div>
                             {error && money.length <= 0 ?
                                 <div className="alert alert-danger" role="alert">
-                                    This field is required!
+                                    Không được để trống ô này!
                                 </div>
                                 : ""}
                             <div className="form-group">
-                                <button type="submit" value="register" id="loginbtn">DEPOSIT</button>
+                                <button type="submit" value="register" id="loginbtn">NẠP TIỀN</button>
                             </div>
                         </form>
                         <div className="redirection">
@@ -77,6 +119,57 @@ function Deposit() {
                     </div>
                 </div>
             </div>
+
+            {showConfirmDialog && (
+                <div className='modal-overlay'>
+                    <div className='modalContainer'>
+                        <h3>Xác nhận</h3>
+                        <p>Vui lòng nhập mật khẩu để xác nhận</p>
+                        <div className="form-group" style={{ display: "inline-flex", justifyContent: "center" }}>
+                            <input
+                                type={showPassword ? 'text' : 'password'}
+                                name="userPassword"
+                                id="userPassword"
+                                className="form-control"
+                                placeholder="Nhập mật khẩu"
+                                value={userPassword}
+                                onChange={(e) => onPasswordChange(e)}
+                            />
+                            <button
+                                type="button"
+                                className="btn btn-outline-secondary"
+                                id='showPasswordBtn'
+                                onClick={toggleShowPassword}
+                            >
+                                {showPassword ? (
+                                    <i className="fa fa-eye"></i>
+                                ) : (
+                                    <i className="fa fa-eye-slash"></i>
+                                )}
+                            </button>
+                        </div>
+                        {wrongPassword &&
+                            <div className="alert alert-danger" role="alert">
+                                Mật khẩu vừa nhập không chính xác!
+                            </div>
+                        }
+                        <div>
+                            <button
+                                className='btn btn-confirm btn-danger'
+                                onClick={handleConfirmTransaction}
+                            >
+                                Xác nhận
+                            </button>
+                            <button
+                                className='btn btn-cancel btn-secondary'
+                                onClick={handleCancel}
+                            >
+                                Hủy
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
--- momo-charity/src/pages/Donate.js
+++ momo-charity/src/pages/Donate.js
@@ -18,8 +18,15 @@ function Donate() {
     const userData = JSON.parse(localStorage.getItem("user"));
 
     const [donateInfo, setDonateInfo] = useState(null);
-    
+
     const params = useParams();
+
+    // handle check password to confirm transation
+    const [showConfirmDialog, setShowConfirmDialog] = useState(false);
+    const [userPhoneNum, setUserPhoneNum] = useState(null);
+    const [userPassword, setUserPassword] = useState(null);
+    const [wrongPassword, setWrongPassword] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
     useEffect(() => {
         showInfo();
@@ -49,13 +56,46 @@ function Donate() {
         navigate(-1);
     }
 
-    const onSubmit = (e) => {
+    // pop-up to ask for credentials
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleTransationClick = (e) => {
         e.preventDefault();
+        setUserPhoneNum(userData.phoneNum);
+        setShowConfirmDialog(true);
+    };
+
+    const onPasswordChange = (e) => {
+        setUserPassword(e.target.value);
+    }
+
+    const handleConfirmTransaction = () => {
+        const user = {
+            'phoneNum': userPhoneNum,
+            'userPassword': userPassword
+        }
+        Axios.post('http://localhost:8080/api/auth/login', user)
+            .then(() => {
+                setShowConfirmDialog(false);
+                onSubmit();
+            }).catch(err => {
+                console.log(err);
+                setWrongPassword(true);
+            });
+    };
+
+    const handleCancel = () => {
+        setShowConfirmDialog(false);
+    };
+
+    const onSubmit = () => {
         if (money.length === 0) {
             setError(true);
         } else {
             Axios.post("http://localhost:8080/api/user/donate", data)
-                .then(res => {
+                .then(() => {
                     setSuccess(true);
                     setMoney("");
                 }).catch(err => console.log(err));
@@ -68,7 +108,7 @@ function Donate() {
                 <div className="form4enter">
                     <div className="row justify-content-center">
                         <h3 id='title_page' className="text-center text-secondary mt-5 mb-3">QUYÊN GÓP</h3>
-                        <form method="POST" onSubmit={(e) => onSubmit(e)}>
+                        <form method="POST" onSubmit={(e) => handleTransationClick(e)}>
                             <div className="form-group">
                                 <label style={{ display: "flex" }} htmlFor="money">Chương trình quyên góp</label>
                                 <input type="url" name="money" id="money" className="form-control" value={donateInfo?.programName} disabled />
@@ -77,14 +117,13 @@ function Donate() {
                                 <label style={{ display: "flex" }} htmlFor="money">Nhập số tiền bạn muốn quyên góp</label>
                                 {/* <input type="number" name="money" id="money" className="form-control" placeholder="0 VND" value={money} onChange={(e) => onInputChange(e)} /> */}
                                 <NumericFormat name="money" id="money" value={money} suffix="đ" displayType="input" type="tel" thousandSeparator="." decimalSeparator="," placeholder="0đ" className={style.money} onValueChange={(values, e) => {
-                                    const {formattedValue, value, floatValue} = values;
-                                    // console.log(values.value);
+                                    const { value } = values;
                                     onInputChange(value);
                                 }} />
                             </div>
                             {error && money.length <= 0 ?
                                 <div className="alert alert-danger" role="alert">
-                                    This field is required!
+                                    Không được để trống ô này!
                                 </div>
                                 : ""}
                             <div className="form-group">
@@ -100,6 +139,57 @@ function Donate() {
                     </div>
                 </div>
             </div>
+
+            {showConfirmDialog && (
+                <div className='modal-overlay'>
+                    <div className='modalContainer'>
+                        <h3>Xác nhận</h3>
+                        <p>Vui lòng nhập mật khẩu để xác nhận</p>
+                        <div className="form-group" style={{ display: "inline-flex", justifyContent: "center" }}>
+                            <input
+                                type={showPassword ? 'text' : 'password'}
+                                name="userPassword"
+                                id="userPassword"
+                                className="form-control"
+                                placeholder="Nhập mật khẩu"
+                                value={userPassword}
+                                onChange={(e) => onPasswordChange(e)}
+                            />
+                            <button
+                                type="button"
+                                className="btn btn-outline-secondary"
+                                id='showPasswordBtn'
+                                onClick={toggleShowPassword}
+                            >
+                                {showPassword ? (
+                                    <i className="fa fa-eye"></i>
+                                ) : (
+                                    <i className="fa fa-eye-slash"></i>
+                                )}
+                            </button>
+                        </div>
+                        {wrongPassword &&
+                            <div className="alert alert-danger" role="alert">
+                                Mật khẩu vừa nhập không chính xác!
+                            </div>
+                        }
+                        <div>
+                            <button
+                                className='btn btn-confirm btn-danger'
+                                onClick={handleConfirmTransaction}
+                            >
+                                Xác nhận
+                            </button>
+                            <button
+                                className='btn btn-cancel btn-secondary'
+                                onClick={handleCancel}
+                            >
+                                Hủy
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
